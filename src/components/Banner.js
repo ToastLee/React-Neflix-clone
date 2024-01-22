0053a7e3e08b5d8663ab7905d51233834200ac18
Isaac Lee <dldltkr970@gmail.com>
2024-01-22 10:18:04 +0900

    비디오 배너 컴포넌트를 생성하고
    Styled 컴포넌트를 이용하여 스타일링,
    
    Ifream 를 넣어 해당 영상의 play를 누를시 유튜브 영상을 틀게끔 비디오 재생을 하게끔 만들었습니다.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,12 @@ import {useEffect, useState} from "react";
 import axios from "../api/axios";
 import requests from "../api/requests";
 import "./Banner.css"
+import styled from "styled-components";
 
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
   
   useEffect(() => {
     fetchData();
@@ -15,7 +17,7 @@ export default function Banner() {
     //현재 상영 중인 영화 정보 가져오기(여러 영화))
     const request = await axios.get(requests.fetchNowPlaying);
     //여러 영화 중 무작위로 하나의 ID 가져오기
-    const movieId = 
+    const movieId =
       request.data.results[
         Math.floor(Math.random() * request.data.results.length)
       ].id;
@@ -30,28 +32,84 @@ export default function Banner() {
   const truncate = (str, n) => {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
+  
 
-
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">More Information</button>
+  //isClicked가 아닐 때
+  if(!isClicked) {
+    return(
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >Play</button>
+            <button className="banner__button info">More Information</button>
+          </div>
+          {/* div 2 buttons */}
+          <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
+          {/* 설명 */}
         </div>
-        {/* div 2 buttons */}
-        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
-        {/* 설명 */}
-      </div>
-      <div className="banner--fadeBottom"/>
-    </header>
-  );
+        <div className="banner--fadeBottom"/>
+      </header>
+    );
+  } else {
+    // 클릭 되었을 때 동영상이 뜨게끔 영상을 띄워주는 부분
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}
+            ?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+          >
+
+          </Iframe>
+        </HomeContainer>
+      </Container>
+    )
+  }
 };
+//Ifram 선언
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  /* opacity: 0.65; */
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
